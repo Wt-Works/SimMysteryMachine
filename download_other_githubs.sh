d32c1f3013e3acebd24ebf0a63e8157f824cdc0a
richelbilderbeek <richel@richelbilderbeek.nl>
2016-11-04 14:57:23 +0100

    Download other GitHubs

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -2,10 +2,10 @@
 
 cd ..
 
-if [ ! -d BoostGraphTutorial ]; then
- git clone https://github.com/richelbilderbeek/BoostGraphTutorial
+if [ ! -d RibiClasses ]; then
+ git clone https://github.com/richelbilderbeek/RibiClasses
 fi
 
-if [ ! -d RibiUnits ]; then
- git clone https://github.com/richelbilderbeek/RibiUnits
+if [ ! -d RibiLibraries ]; then
+ git clone https://github.com/richelbilderbeek/RibiLibraries
 fi
